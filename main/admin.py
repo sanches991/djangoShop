a1c2cf6e1388e98cc0d8a00c66a8d455e918ed70
Sanches Sanches <sanches0_1@mail.ru>
2026-03-09 22:05:45 +0600

    сделал views, admin , urls

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,28 @@
 from django.contrib import admin
+from .models import Category, Size, Product,\
+ProductImage, ProductSize
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+    list_filter = ['category' , 'color']
+    search_fields = ['name', 'color' , 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductSizeInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
